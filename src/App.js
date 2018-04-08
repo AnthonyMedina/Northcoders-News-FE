ec62f58a19478d444051d5cd00a58c99fc0c41d6
AnthonyMedina <antmedina91@gmail.com>
2018-04-08 15:54:39 +0100

    create /topics route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Article from "./components/Article";
+import Topic from "./components/Topic";
 import "./App.css";
 
 const App = () => (
@@ -9,6 +10,7 @@ const App = () => (
     <Fragment>
       <Header />
       <Route path="/articles" component={Article.Container} />
+      <Route path="/topics" component={Topic.Container} />
     </Fragment>
   </Router>
 );
---
+++ src/components/Topic.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import Article from "./Article.js";
+
+const Topic = {
+  Container: class Container extends Component {
+    state = {
+      topics: []
+    };
+    render() {
+      const { match } = this.props;
+      return (
+        <main className="container">
+          <Route
+            exact
+            path={`${match.url}`}
+            render={props => <Topic.List {...props} />}
+          />
+        </main>
+      );
+    }
+  },
+  List: props => (
+    <main className="container">
+      <Topic.Card />
+    </main>
+  ),
+  Card: props => (
+    <div class="col-12 col-md-4 shadow my-3 p-3">
+      <img
+        class="card-img-top"
+        src="https://source.unsplash.com/collection/190727/300x300"
+        alt="Article"
+      />
+      <div class="card-body">
+        <p class="card-text">Topic Title</p>
+      </div>
+    </div>
+  )
+};
+
+export default Topic;

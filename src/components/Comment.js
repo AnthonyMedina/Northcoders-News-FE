f4ed086e1c7004d7e170d2bf5ae9af3c600c8d91
AnthonyMedina <antmedina91@gmail.com>
2018-04-11 14:28:56 +0100

    add post comment functionality to comment input

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -10,15 +10,28 @@ const Comment = {
     state = {
       comments: []
     };
+
     componentDidMount() {
       API.fetchComments(this.props.article_id).then(comments => {
         this.setState({ comments });
       });
     }
+
+    postComment = (article_id, input) => {
+      API.postComment(article_id, input).then(comment =>
+        this.setState({
+          comments: [comment, ...this.state.comments]
+        })
+      );
+    };
+
     render() {
       return (
         <Fragment>
-          <Comment.Input />
+          <Comment.Input
+            article_id={this.props.article_id}
+            postComment={this.postComment}
+          />
           {this.state.comments.map((comment, i) => {
             return <Comment.Card key={i} comment={comment} />;
           })}
@@ -28,23 +41,59 @@ const Comment = {
 
     static propTypes = {};
   },
-  Input: props => {
-    return (
-      <div className="row shadow p-3 my-3">
-        <div className="col-2  d-flex align-items-center justify-content-center">
-          Avatar
+  Input: class Input extends Component {
+    state = {
+      commentInput: ""
+    };
+
+    handleClick = e => {
+      if (this.state.commentInput.length > 0) {
+        this.props.postComment(this.props.article_id, this.state.commentInput);
+        this.setState({
+          commentInput: ""
+        });
+      }
+    };
+
+    handleChange = e => {
+      this.setState({
+        commentInput: e.target.value
+      });
+    };
+
+    render() {
+      return (
+        <div className="row shadow p-3 my-3">
+          <div className="col-2  d-flex align-items-center justify-content-center">
+            Avatar
+          </div>
+          <div className="col-10  d-flex align-items-center justify-content-between">
+            <input
+              type="text"
+              height="100"
+              placeholder="Write a response..."
+              onChange={this.handleChange}
+              value={this.state.commentInput}
+            />
+            <button
+              type="button"
+              className="btn btn-outline-secondary"
+              onClick={this.handleClick}
+            >
+              Publish
+            </button>
+          </div>
         </div>
-        <div className="col-10  d-flex align-items-center justify-content-between">
-          <input type="text" height="100" placeholder="Write a response..." />
-          <button type="button" className="btn btn-outline-secondary">
-            Publish
-          </button>
-        </div>
-      </div>
-    );
+      );
+    }
   },
   Card: ({ comment }) => {
+    const deleteComment = comment_id => {
+      // API.deleteComment
+    };
+
     if (!comment) return null;
+
     return (
       <div className="row shadow p-3 my-3">
         <div className="col-2 d-flex flex-column justify-content-around align-items-center">
